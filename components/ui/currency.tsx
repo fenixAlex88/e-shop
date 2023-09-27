41bb7be844ae86d8ebe9a4b4e6548afaea8fa3cf
Aleksey Pekar <fenix_al@tut.by>
2023-09-27 18:32:46 +0300

    create shop

---
+++ components/ui/currency.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+
+import { formatter } from "@/lib/utils";
+
+interface CurrencyProps {
+    value: string | number;
+}
+
+export const Currency: React.FC<CurrencyProps> = ({ value }) => {
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    if (!isMounted) return null;
+    return <div className="font-semibold">{formatter.format(Number(value))}</div>;
+};
